ae9ffd39b2ad089f5a7571256e41081500e5692e
Haochi Chen <haochi.chen@gmail.com>
2013-10-16 00:49:05 -0700

    remove unnecessary escapings

--- test/test.js
+++ test/test.js
@@ -20,26 +20,46 @@ describe('Queries from Query 101', function(){
   describe('=', function(){
     it('should filter where city = Seattle', function(){
       var query = q().where(attr("city").equal("Seattle")).to_sql()
-      assert.equal(query, "SELECT * FROM mydomain WHERE `city` = 'Seattle'");
+      assert.equal(query, "SELECT * FROM mydomain WHERE city = 'Seattle'");
     })
 
     it('should filter where city = Seattle or city = Portland', function(){
       var city = attr("city")
       var query = q().where(city.equal("Seattle").or(city.equal("Portland"))).to_sql()
-      assert.equal(query, "SELECT * FROM mydomain WHERE `city` = 'Seattle' OR `city` = 'Portland'");
+      assert.equal(query, "SELECT * FROM mydomain WHERE city = 'Seattle' OR city = 'Portland'");
     })
   })
 
   describe('!=', function(){
     it('should filter where name != John', function(){
       var query = q().where(attr("name").equal("John")).to_sql()
-      assert.equal(query, "SELECT * FROM mydomain WHERE `name` = 'John'");
+      assert.equal(query, "SELECT * FROM mydomain WHERE name = 'John'");
     })
 
     it('should filter where name != John and name != Humberto', function(){
       var name = attr("name")
       var query = q().where(name.not_equal("John").and(name.not_equal("Humberto"))).to_sql()
-      assert.equal(query, "SELECT * FROM mydomain WHERE `name` != 'John' AND `name` != 'Humberto'");
+      assert.equal(query, "SELECT * FROM mydomain WHERE name != 'John' AND name != 'Humberto'");
+    })
+  })
+
+  describe('quoting', function(){
+    it('reserved keywords should be quoted', function(){
+      var where = attr("where")
+      var query = q().where(where.not_equal("USA").and(where.not_equal("Canada"))).to_sql()
+      assert.equal(query, "SELECT * FROM mydomain WHERE `where` != 'USA' AND `where` != 'Canada'");
+    })
+
+    it('not standard identifiers should be quoted', function(){
+      var where = attr("1World1Dream")
+      var query = q().where(where.not_equal("true")).to_sql()
+      assert.equal(query, "SELECT * FROM mydomain WHERE `1World1Dream` != 'true'");
+    })
+
+    it("values with one single quote should be properly escaped", function(){
+      var last_name = attr("last_name")
+      var query = q().where(last_name.like("O'Neal%")).to_sql()
+      assert.equal(query, "SELECT * FROM mydomain WHERE last_name LIKE 'O''Neal%'");
     })
   })
 
@@ -56,7 +76,7 @@ describe('More Complex Queries', function(){
       ).and(score.equal("100"))
     ).to_sql()
 
-    assert.equal(query, "SELECT * FROM mydomain WHERE `name` = 'John' OR (`name` = 'Ben' AND `email` = 'example@example.com') AND `score` = '100'")
+    assert.equal(query, "SELECT * FROM mydomain WHERE name = 'John' OR (name = 'Ben' AND email = 'example@example.com') AND score = '100'")
   })
 })
 
