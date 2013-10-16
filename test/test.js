4a329385fd10c0463f54b027e662578bde48a9f2
Haochi Chen <haochi.chen@gmail.com>
2013-10-16 00:22:07 -0700

    remove redundant logical parenthesis

--- test/test.js
+++ test/test.js
@@ -26,7 +26,7 @@ describe('Queries from Query 101', function(){
     it('should filter where city = Seattle or city = Portland', function(){
       var city = attr("city")
       var query = q().where(city.equal("Seattle").or(city.equal("Portland"))).to_sql()
-      assert.equal(query, "SELECT * FROM mydomain WHERE `city` = 'Seattle' OR (`city` = 'Portland')");
+      assert.equal(query, "SELECT * FROM mydomain WHERE `city` = 'Seattle' OR `city` = 'Portland'");
     })
   })
 
@@ -39,10 +39,25 @@ describe('Queries from Query 101', function(){
     it('should filter where name != John and name != Humberto', function(){
       var name = attr("name")
       var query = q().where(name.not_equal("John").and(name.not_equal("Humberto"))).to_sql()
-      assert.equal(query, "SELECT * FROM mydomain WHERE `name` != 'John' AND (`name` != 'Humberto')");
+      assert.equal(query, "SELECT * FROM mydomain WHERE `name` != 'John' AND `name` != 'Humberto'");
     })
   })
 
+})
+
+describe('More Complex Queries', function(){
+  it('should nest correctly', function(){
+    var name = attr("name")
+      , email = attr("email")
+      , score = attr("score")
+    var query = q().where(
+      name.equal("John").or(
+        name.equal("Ben").and(email.equal("example@example.com"))
+      ).and(score.equal("100"))
+    ).to_sql()
+
+    assert.equal(query, "SELECT * FROM mydomain WHERE `name` = 'John' OR (`name` = 'Ben' AND `email` = 'example@example.com') AND `score` = '100'")
+  })
 })
 
 
